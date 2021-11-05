973a01c024881c651e6f26c7ff437d5a95f5db5c
Elisabeth Udyawar <elisabeth.bass@gmail.com>
2021-11-04 22:15:47 -0400

    work on controllers

--- tenmo-server/src/main/java/com/techelevator/tenmo/controller/TenmoController.java
+++ tenmo-server/src/main/java/com/techelevator/tenmo/controller/TenmoController.java
@@ -5,10 +5,7 @@ import com.techelevator.tenmo.dao.TransferDao;
 import com.techelevator.tenmo.dao.UserDao;
 import com.techelevator.tenmo.model.Transfer;
 import com.techelevator.tenmo.model.User;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.math.BigDecimal;
 import java.util.List;
@@ -34,26 +31,26 @@ public class TenmoController {
 
     //getting individual balance
     @RequestMapping(path = "/account/{id}", method = RequestMethod.GET)
-        public BigDecimal accountBalance(@PathVariable Long accountId) { // I dont like how I used the reference type Long, might change it
+        public BigDecimal accountBalance(@PathVariable("id") Long accountId) { // I dont like how I used the reference type Long, might change it
         return accountDao.getBalance(accountId);
     }
 
     //getting transfer history
     @RequestMapping(path ="/transfer", method = RequestMethod.GET)
-    public List<Transfer> transferHistory(@PathVariable Long accountId){
+    public List<Transfer> transferHistory(@RequestParam Long accountId){
         return transferDao.getTransferHistory(accountId);
     }
     //I feel like there could be some ambiguity with accountId or maybe I'm just confused like usual lol
 
     // POST new transfer
     @RequestMapping(path = "/transfer", method = RequestMethod.POST)
-    public String transferDetails(@PathVariable BigDecimal amount, @PathVariable Long accountFrom, @PathVariable Long accountTo){
+    public String transferDetails(BigDecimal amount, Long accountFrom, Long accountTo){
         return transferDao.send(amount, accountFrom, accountTo);
     }
 
     // GET individual transfer details
-    @RequestMapping(path = "/transfer", method = RequestMethod.GET)
-    public Transfer getSingleTransferDetails(Long transferId){
+    @RequestMapping(path = "/transfer/{id}", method = RequestMethod.GET)
+    public Transfer SingleTransferDetails(@PathVariable("id") Long transferId){
         return transferDao.getSingleTransferDetails(transferId);
     }
 }
