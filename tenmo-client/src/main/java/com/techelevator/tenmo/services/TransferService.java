d2fdfcc605e4c8a99978266fdaa0a76b14a1ee8d
Grant <grant.sapir@gmail.com>
2021-11-07 18:10:07 -0500

    send bucks

--- tenmo-client/src/main/java/com/techelevator/tenmo/services/TransferService.java
+++ tenmo-client/src/main/java/com/techelevator/tenmo/services/TransferService.java
@@ -1,5 +1,7 @@
 package com.techelevator.tenmo.services;
 
+import com.techelevator.tenmo.model.AuthenticatedUser;
+import com.techelevator.tenmo.model.Transfer;
 import com.techelevator.tenmo.model.User;
 import org.springframework.http.*;
 import org.springframework.web.client.ResourceAccessException;
@@ -7,6 +9,7 @@ import org.springframework.web.client.RestClientResponseException;
 import org.springframework.web.client.RestTemplate;
 
 import java.math.BigDecimal;
+import java.util.Scanner;
 
 public class TransferService {
 
@@ -14,7 +17,7 @@ public class TransferService {
     private final RestTemplate restTemplate = new RestTemplate();
     //there is not what we know as a "console service" because the app
     // is handling the cli somehow so I'm not sure what to do
-
+    private AuthenticatedUser currentUser;
     private String authToken = null;
 
     public TransferService(String url) {
@@ -68,6 +71,31 @@ public class TransferService {
         }
         return users;
 
+    }
+    public void sendBucks() {
+        Transfer transfer = new Transfer();
+        System.out.println("Enter ID of user you are sending to (0 to cancel): ");
+        Scanner scanner = new Scanner(System.in);
+        transfer.setAccountTo(Long.parseLong(scanner.nextLine()));
+
+        if(transfer.getAccountTo() != 0){
+            System.out.println("Amount to send: ");
+            try {
+                transfer.setAmount(new BigDecimal(Double.parseDouble(scanner.nextLine())));
+            } catch (NumberFormatException e) {
+                System.out.println("ERROR");
+            }
+            String response = restTemplate.exchange(baseUrl + "transfer", HttpMethod.POST, makeTransferEntity(transfer), String.class).getBody();
+            System.out.println(response);
+        }
+    }
+
+    private HttpEntity<Transfer> makeTransferEntity(Transfer transfer) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        headers.setBearerAuth(authToken);
+        HttpEntity<Transfer> entity = new HttpEntity<>(transfer, headers);
+        return entity;
     }
 
     private HttpEntity<Void> makeAuthEntity() {
